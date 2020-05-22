4183b152ab8ad207fd29acefc79be60c536687f5
Serhii <vasylyk29@gmail.com>
2020-05-22 18:02:21 +0300

    Updates

---
+++ precache-manifest.386b16c0cb0b081634094b2fb184efd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89f63e6a56d5a594c29bb5f8068a3dd1",
+    "url": "/pokemons/index.html"
+  },
+  {
+    "revision": "fdb6ace376e9d0d74e8f",
+    "url": "/pokemons/static/css/main.af9c23bc.chunk.css"
+  },
+  {
+    "revision": "d8aeac191f12eca8ae4e",
+    "url": "/pokemons/static/js/2.cc85bf20.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokemons/static/js/2.cc85bf20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdb6ace376e9d0d74e8f",
+    "url": "/pokemons/static/js/main.fbe2e8d4.chunk.js"
+  },
+  {
+    "revision": "1653efcf83f035a03274",
+    "url": "/pokemons/static/js/runtime-main.5fe0cb78.js"
+  }
+]);
